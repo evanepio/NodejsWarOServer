eaf8d0b4fd55038b4233d3de99631010c1606a0b
Evan Porter <evan.porter@dotman.ca>
2016-07-09 22:07:00 -0300

    Removed accidentally left in function.

--- src/core.js
+++ src/core.js
@@ -4,7 +4,3 @@ import {v4} from 'node-uuid';
 export function createPlayer(name) {
     return {name, id: v4(), hand: List.of()};
 }
-
-export function addPlayer(state, player) {
-    return state.update(player.id, player);
-}
