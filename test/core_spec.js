31e00c503a768b728ed563957d643283349ec710
Evan Porter <evan.porter@dotman.ca>
2016-07-11 18:12:33 -0300

    Shouldn't use arrow functions in mocha.js (prevents access to mocha context)

--- test/core_spec.js
+++ test/core_spec.js
@@ -4,34 +4,34 @@ import {expect} from 'chai';
 import {createGame, createPlayer} from '../src/core';
 
 
-describe('createGame', () => {
-    it('sets default player map', () => {
+describe('createGame', function() {
+    it('sets default player map', function() {
         const game = createGame();
 
         expect(game.get('players')).to.equal(Map({}));
     });
-    it('sets an empty player map', () => {
+    it('sets an empty player map', function() {
         const game = createGame();
 
         expect(game.get('players').count()).to.equal(0);
     });
 });
 
-describe('createPlayer', () => {
-    it('sets name properly', () => {
+describe('createPlayer', function() {
+    it('sets name properly', function() {
         const name = 'someName';
         const player = createPlayer(name);
 
         expect(player.name).to.equal(name);
     });
 
-    it('sets a non-empty ID', () => {
+    it('sets a non-empty ID', function() {
         const player = createPlayer('someName');
 
         expect(player.id).to.exist;
     });
 
-    it('initializes an empty hand', () => {
+    it('initializes an empty hand', function() {
         const player = createPlayer('someName');
 
         expect(player.hand).to.equal(List.of());
